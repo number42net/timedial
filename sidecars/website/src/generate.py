69e1402f45bd892cfaf5e2d86ab612d0894c6e5c
number42net <github@number42.net>
2025-06-01 20:25:00 +0200

    Improve website

--- sidecars/website/src/generate.py
+++ sidecars/website/src/generate.py
@@ -20,11 +20,41 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 from pathlib import Path
 
 import markdown
+import requests
 from jinja2 import Template
 
 CONTENT_DIR = Path("content")
 TEMPLATE_PATH = Path("base.html")
 OUTPUT_DIR = Path("../html")
+
+
+def validate(html: str) -> None:
+    """Validate the generated HTML using w3.org api."""
+    files = files = {"uploaded_file": ("test.html", html, "text/html; charset=us-ascii")}
+    response = requests.post("https://validator.w3.org/check", files=files, data={"doctype": "Inline", "output": "json"})
+
+    result = response.json()
+    if result["messages"]:
+        for message in result["messages"]:
+            for key, value in message.items():
+                print(f"## {key}")
+                print(value)
+            print()
+        exit(1)
+
+
+def clean_up(html: str) -> str:
+    """Clean-up the HTML with indendations and fix links."""
+    final = []
+    for line in html.split("\n"):
+        # Replace .md links to .html
+        line = line.replace('.md">', '.html">')
+        # Replace html5 <br /> with html2 <br>
+        line = line.replace("<br />", "<br>")
+        final.append(line)
+
+    return "\n".join(final)
+
 
 # Load HTML template
 with open(TEMPLATE_PATH, encoding="utf-8") as f:
@@ -32,37 +62,40 @@ with open(TEMPLATE_PATH, encoding="utf-8") as f:
 
 OUTPUT_DIR.mkdir(exist_ok=True)
 
+# Load list of files
+files = list(CONTENT_DIR.glob("*.md"))
+files.sort()
 
-def clean_up(html: str) -> str:
-    """Clean-up the HTML with indendations and fix links."""
-    final = []
-    for line in html.split("\n"):
-        line = line.replace('.md">', '.html">')
-        line = line.replace("<br />", "<br>")
-        if line.strip()[:-2].endswith("</h"):
-            line = "\n" + line
-        final.append(line)
+# Generate all pages, ensure Index is always first
+all_pages = [i.stem.capitalize() for i in files]
+all_pages.remove("Index")
+all_pages.insert(0, "Index")
 
-    return "\n".join(final)
-
-
-for md_file in CONTENT_DIR.glob("*.md"):
+for md_file in files:
     print(f"Processing: {md_file}")
     try:
+        # Read markdown file
         with open(md_file, encoding="utf-8") as f:
             md_content = f.read()
 
+        # Check for non-ascii characters
         for number, line in enumerate(md_content.split("\n")):
             if not line.isascii():
                 result = [(i, c) for i, c in enumerate(line) if ord(c) >= 128]
                 print(f"Found non-ascii character on line: {number + 1} col: {result[0][0] + 1}: {result[0][1]}")
                 exit(1)
 
+        # Convert Markdown to THML
         html_content = markdown.markdown(md_content, extensions=["fenced_code"])
 
-        title = f"TimeDial {md_file.stem.capitalize()}"
-        rendered_html = base_template.render(title=title, content=clean_up(html_content))
+        # Create page title and generate HTML
+        title = md_file.stem.capitalize()
+        rendered_html = base_template.render(title=title, all_pages=all_pages, content=clean_up(html_content))
 
+        # Validate output
+        validate(rendered_html)
+
+        # Write output
         output_path = OUTPUT_DIR / f"{md_file.stem}.html"
         with open(output_path, "w", encoding="ascii") as f:
             f.writelines(rendered_html)
