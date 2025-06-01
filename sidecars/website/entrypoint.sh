f28aa9711355b3721a97eded0da01de893f353b3
number42net <github@number42.net>
2025-06-01 16:53:30 +0200

    Add website

---
+++ sidecars/website/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+trap "echo 'Caught SIGTERM, exiting...'; exit 0" TERM INT
+
+if [[ -f /etc/letsencrypt/live/timedial.org/fullchain.pem ]]; then
+    certbot renew
+else
+    certbot certonly --non-interactive --standalone --email toor@timedia.org --agree-tos --no-eff-email -d timedial.org
+fi
+
+nginx -g "daemon off;"
